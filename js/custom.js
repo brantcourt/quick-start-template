91153210f9e0735ee66f8a8742e70a5182b7095b
Brant <brant.court@hotmail.com>
2019-05-13 14:07:21 -0400

    First Commit, Bootstrap 4 starter template

---
+++ js/custom.js
@@ -0,0 +1,81 @@
+jQuery(document).ready(function( $ ) {
+
+  // Preloader
+  $(window).on('load', function() {
+    $('#preloader').delay(100).fadeOut('slow',function(){$(this).remove();});
+  });
+
+  // Hero rotating texts
+  $("#hero .rotating").Morphext({
+    animation: "flipInX",
+    separator: ",",
+    speed: 3000
+  });
+  
+  // Initiate the wowjs
+  new WOW().init();
+  
+  // Initiate superfish on nav menu
+  $('.nav-menu').superfish({
+    animation: {opacity:'show'},
+    speed: 400
+  });
+  
+  // Stick the header at top on scroll
+  $("#header").sticky({topSpacing:0, zIndex: '50'});
+
+  // Smoth scroll on page hash links
+  $('a[href*="#"]:not([href="#"])').on('click', function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          if (target.length) {
+              
+              var top_space = 0;
+              
+              if( $('#header').length ) {
+                top_space = $('#header').outerHeight();
+              }
+              
+              $('html, body').animate({
+                  scrollTop: target.offset().top - top_space
+              }, 1500, 'easeInOutExpo');
+
+              if ( $(this).parents('.nav-menu').length ) {
+                $('.nav-menu .menu-active').removeClass('menu-active');
+                $(this).closest('li').addClass('menu-active');
+              }
+
+              if ( $('body').hasClass('mobile-nav-active') ) {
+                  $('body').removeClass('mobile-nav-active');
+                  $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+                  $('#mobile-body-overly').fadeOut();
+              }
+              
+              return false;
+          }
+      }
+  });
+
+$(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+  
+  // Back to top button
+  $(window).scroll(function() {
+
+      if ($(this).scrollTop() > 100) {
+          $('.back-to-top').fadeIn('slow');
+      } else {
+          $('.back-to-top').fadeOut('slow');
+      }
+      
+  });
+  
+  $('.back-to-top').click(function(){
+      $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+      return false;
+  });
+
+});
